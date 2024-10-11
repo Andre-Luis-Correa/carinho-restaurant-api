0f562693c6389cd9576589e46b0091181eb34c1a
Andre Luis Correa <andreluiscorrea2003@gmail.com>
2024-10-11 14:11:41 -0300

    feat: start deploy

--- src/main/java/com/menumaster/restaurant/RestaurantApplication.java
+++ src/main/java/com/menumaster/restaurant/RestaurantApplication.java
@@ -5,13 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class RestaurantApplication {
 
-	@RequestMapping("/home")
-	public String home() {
-		return "Spring Boot Application for restaurant Carinho";
+	@RestController
+	class HelloworldController {
+		@GetMapping("/")
+		String home() {
+		return "home!";
+		}
 	}
 
 	public static void main(String[] args) {
